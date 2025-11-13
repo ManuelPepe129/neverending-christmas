795c0c5790193c54662f00db5fc4ee63cd468f16
ManuelPepe129 <manuelpepe1995@gmail.com>
2025-11-13 17:15:37 +0100

    Add collectables handling

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,27 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     [SerializeField] private float dayDuration = 10.0f;
     private float _dayDuration;
     private float _currentTime;
+    private Coroutine _dayTimerCoroutine;
 
+    [SerializeField] private GameObject[] giftSpawnPositions;
+    [SerializeField] private GameObject giftToSpawn;
+    [SerializeField] private int numberOfGiftsToSpawn;
+    
     private int _totalGifts = 0;
     private int _giftsCollected = 0;
 
     void Start()
     {
-        // TODO: Get objects in scene
+        // TODO: Move to UI
+        StartGame();
     }
 
     void StartGame()
@@ -21,8 +29,22 @@ public class NewMonoBehaviourScript : MonoBehaviour
         // Start timer
         _dayDuration = dayDuration * 60f; // Convert minutes to seconds
         _currentTime = 0f;
-        // Spawn gifts (?)
-        // Spawn enemies
+        _dayTimerCoroutine = StartCoroutine(DayTimer());
+        
+        // Spawn gifts
+        var tmpGiftSpawnPositions = new List<GameObject>(giftSpawnPositions);
+        if (numberOfGiftsToSpawn > giftSpawnPositions.Length) numberOfGiftsToSpawn = giftSpawnPositions.Length;
+        int giftSpawned = 0;
+        while (giftSpawned < numberOfGiftsToSpawn)
+        {
+            var positionIndex = Random.Range(0, tmpGiftSpawnPositions.Count);
+            var spawnPosition = tmpGiftSpawnPositions[positionIndex].transform.position;
+            Instantiate(giftToSpawn, spawnPosition, Quaternion.identity);
+            tmpGiftSpawnPositions.RemoveAt(positionIndex);
+            giftSpawned++;
+        }
+        
+        // Start Waves
     }
 
     private IEnumerator DayTimer()
@@ -32,6 +54,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
             _currentTime += Time.deltaTime;
             yield return new WaitForSeconds(1.0f);
         }
+        // TODO: Game Over
+        Debug.Log("Current day ended");
     }
     
     public void OnGiftCollected()
@@ -43,11 +67,17 @@ public class NewMonoBehaviourScript : MonoBehaviour
             // TODO: Spawn Boss wave
         }
     }
+
+    private void SpawnBossWave()
+    {
+        throw new NotImplementedException("SpawnBossWave not implemented yet.");
+    }
     
     public void OnPlayerDeath(bool isDeath)
     {
         // Play Game Over audio
         // Update UI
+        StopCoroutine(_dayTimerCoroutine);
         throw new NotImplementedException("OnPlayerDeath event not implemented yet.");
         Time.timeScale = 0; // Freeze the game
     }
@@ -55,6 +85,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public void OnLevelCompleted()
     {
         // Play Win audio
+        StopCoroutine(_dayTimerCoroutine);
         throw new NotImplementedException("OnLevelCompleted event not implemented yet.");
         Time.timeScale = 0; // Freeze the game
     }
