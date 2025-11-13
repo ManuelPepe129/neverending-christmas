190e030c6c234dc8b6d5d09af22090687ab467f3
Chiara De Giorgi <chiaradg@lecceinrete.it>
2025-11-13 23:09:44 +0100

    NavMesh

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private NavMeshAgent _enemy;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _enemy.SetDestination(player.position);
+    }
+}
